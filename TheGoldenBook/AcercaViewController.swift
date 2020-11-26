ebc6e21564f93c46cf0c457df2b9d33af8a9e04d
Ricardo Velazquez <ricardov_5678@hotmail.com>
2020-11-26 00:39:11 -0600

    Aactualización de colores y fotos

---
+++ TheGoldenBook/AcercaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AcercaViewController.swift
+//  TheGoldenBook
+//
+//  Created by user181378 on 11/26/20.
+//
+
+import UIKit
+
+class AcercaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [UIColor.white.cgColor, UIColor.orange.cgColor]
+        
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard(){
+        view.endEditing(true)
+    }
+
+}
---
+++ TheGoldenBook/LibroViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LibroViewController.swift
+//  TheGoldenBook
+//
+//  Created by user181378 on 11/26/20.
+//
+
+import UIKit
+
+class LibroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [UIColor.white.cgColor, UIColor.orange.cgColor]
+        
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
