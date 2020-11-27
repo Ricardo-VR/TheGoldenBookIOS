cb1239dcfa987aba8ca6f76367aadace55ff7a69
Ricardo Velazquez <ricardov_5678@hotmail.com>
2020-11-26 18:56:26 -0600

    Finished flow

---
+++ TheGoldenBook/BorrarLibroViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BorrarLibroViewController.swift
+//  TheGoldenBook
+//
+//  Created by user181378 on 11/26/20.
+//
+
+import UIKit
+
+class BorrarLibroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closeModal(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
